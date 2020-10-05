4613a3199d3b486c65ac587d2434a3c9171d24c2
Yash Bansal <agarwaly482@gmail.com>
2020-10-05 20:14:19 +0530

    added js file for deleting files asynchronously

---
+++ public/js/deleteProduct.js
@@ -0,0 +1,20 @@
+ const deleteProduct = btn => {
+     
+     const prodId = btn.parentNode.querySelector('[name = productId]').value;
+     const csrf = btn.parentNode.querySelector('[name = _csrf]').value;
+     const productElement = btn.closest('article');
+
+    fetch('/admin/product/' + prodId, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token' : csrf
+        }
+    })
+    .then(result => {
+        console.log(result);
+        productElement.parentNode.removeChild(productElement);
+    })
+    .catch(err => {
+        console.log('error');
+    });
+ }
