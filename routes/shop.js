93f70ca5e61a0d3abf4f95b86eaf118303dd2ac4
Yash Bansal <agarwaly482@gmail.com>
2020-10-06 23:33:22 +0530

    added payment functionality

--- routes/shop.js
+++ routes/shop.js
@@ -15,11 +15,15 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', isAuth, shopController.getCart);
 
-router.post('/cart', isAuth, shopController.postCart);
+router.post('/cart', isAuth, shopController.postCart)
+
+router.get('/checkout', isAuth, shopController.getCheckout);
+
+router.get('/checkout/success', isAuth, shopController.postOrder);
+
+router.get('/checkout/cancel', isAuth, shopController.getCheckout);
 
 router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
-
-router.post('/create-order', isAuth, shopController.postOrder);
 
 router.get('/orders', isAuth, shopController.getOrders);
 
